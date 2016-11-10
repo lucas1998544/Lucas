d018c04372ae7fece36e91ff8fc2a756213baf06
lucas1998544 <lucas1998544@gmail.com>
2016-11-10 09:34:58 -0200

    Add files via upload

---
+++ usuario.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>La Prima</title>
+    
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Logart Comunicação Visual e uma empresa que faz o layout da sua arte e ainda entrega impresso!">
+	<meta name="keywords" content="logart, logart cominic, logart adesivos, logart comunicaçao visual, logart engenho novo, logart barueri, cartao de visita logart, coberturas logart, contato logart ">
+	<meta name="author" content="Lucas Lopes">
+        <link rel="shortcut icon" href="images/logoxx.png">
+        
+        
+        <!-- padrão geral --> 
+        <link rel="stylesheet" href="css/default.css"> 
+        <!-- medias convencionais responsivas basicas USAR APENAS QUANDO NÃO SE QUER ORIENTAÇÕES  -->
+        <link rel="stylesheet" media="screen and (max-width:319px)" href="css/style_319.css">
+        <link rel="stylesheet" media="screen and (min-width:320px) and (max-width:479px)" href="css/style_320_479.css">
+        <link rel="stylesheet" media="screen and (min-width:480px) and (max-width:599px)" href="css/style_480_599.css">
+        <link rel="stylesheet" media="screen and (min-width:600px) and (max-width:767px)" href="css/style_600_767.css">
+        <link rel="stylesheet" media="screen and (min-width:768px) and (max-width:1023px)" href="css/style_768_1023.css">
+        <link rel="stylesheet" media="screen and (min-width:1024px) and (max-width:1199px)" href="css/style_1024_1199.css">
+        <link rel="stylesheet" media="screen and (min-width:1200px) and (max-width:1365px)" href="css/style_1200_1365.css">
+        <link rel="stylesheet" media="screen and (min-width:1366px)" href="css/style_1366.css">
+        
+        <script src="js/jquery-3.1.0.min.js" type="text/javascript"></script>
+	
+
+    </head>
+    
+    <body>
+        
+        <div id="barraTopo">
+            
+             <section id="conteudoBarraTopo">
+                    
+                    <p class="numeros">(11) 4375-0807</p>
+                    <p class="numeros">(11) 4375-0807</p> 
+                    <p class="numeros">(11) 4375-0807</p> 
+                    
+                    <form id="formBarra">
+                        <p class="loginBarra">Usuario &nbsp;<input class="caixaTxtBarra" type="text" placeholder="Usuario"></p>
+                        
+                        <p class="loginBarra">Senha &nbsp;<input class="caixaTxtBarra" type="text" placeholder="Senha"></p>
+                        <input type="submit" id="botaoEntrarBarra" value="Entrar">
+                        <a href="" id="segBotaoCadastroBarra" ><p id="botaoCadastroBarra">Cadastre-se</p></a>
+                    </form>
+                    
+                </section> 
+            
+            
+            
+        </div>
+        
+        <section id="containner">
+            
+          
+            
+            
+            
+            <header>
+                
+                
+                
+                 
+                
+                
+                
+                
+                
+                  
+                
+                
+                
+                
+                
+                
+              <a href="index.php">
+                
+                <picture>
+          
+      
+      <source media="(min-width:1366px)" class="logo" type="image/webp" srcset="img/logo.png">    
+      <source media="(min-width:1366px)" class="logo" type="image/jpg" srcset="img/logo_corte2.png">
+      
+      
+      <source media="(min-width:1199px)" class="logo" type="image/webp" srcset="img/logo_corte3.webp">
+      <source media="(min-width:1199px)" class="logo" type="image/jpg" srcset="img/logo_corte3.png">
+      
+      
+      <source media="(min-width:1023px)" class="logo" type="image/webp" srcset="img/logo_corte4.webp">
+      <source media="(min-width:1023px)" class="logo" type="image/jpg" srcset="img/logo_corte4.png">
+      
+      
+      <source media="(min-width:767px)" class="logo" type="image/webp" srcset="img/logo_corte5.webp">
+      <source media="(min-width:767px)" class="logo" type="image/jpg" srcset="img/logo_corte5.png">
+      
+      
+      <source media="(min-width:599px)" class="logo" type="image/webp" srcset="img/logo_corte6.webp">
+      <source media="(min-width:599px)" class="logo" type="image/jpg" srcset="img/logo_corte6.png">
+      
+      
+      <source media="(min-width:479px)" class="logo" type="image/webp" srcset="img/logo_corte7.webp">
+      <source media="(min-width:479px)" class="logo" type="image/jpg" srcset="img/logo_corte7.png">
+      
+      
+      <source media="(min-width:319px)" class="logo" type="image/webp" srcset="img/logo_corte8.webp">
+      <source media="(max-width:319px)" class="logo" type="image/jpg" srcset="img/logo_corte8.png">
+      
+   
+   <img src="img/logo_corte1.png" class="logo" alt="Logo">  
+   
+      
+               
+      
+      
+      
+      
+      </picture> 
+                </a>
+                
+                
+                
+                <picture>
+          
+      
+      <source media="(min-width:1366px)"  type="image/webp" srcset="img/header.jpg">    
+      <source media="(min-width:1366px)" class="logo" type="image/jpg" srcset="img/logo_corte2.png">
+      
+      
+      <source media="(min-width:1199px)" class="logo" type="image/webp" srcset="img/logo_corte3.webp">
+      <source media="(min-width:1199px)" class="logo" type="image/jpg" srcset="img/logo_corte3.png">
+      
+      
+      <source media="(min-width:1023px)" class="logo" type="image/webp" srcset="img/logo_corte4.webp">
+      <source media="(min-width:1023px)" class="logo" type="image/jpg" srcset="img/logo_corte4.png">
+      
+      
+      <source media="(min-width:767px)" class="logo" type="image/webp" srcset="img/logo_corte5.webp">
+      <source media="(min-width:767px)" class="logo" type="image/jpg" srcset="img/logo_corte5.png">
+      
+      
+      <source media="(min-width:599px)" class="logo" type="image/webp" srcset="img/logo_corte6.webp">
+      <source media="(min-width:599px)" class="logo" type="image/jpg" srcset="img/logo_corte6.png">
+      
+      
+      <source media="(min-width:479px)" class="logo" type="image/webp" srcset="img/logo_corte7.webp">
+      <source media="(min-width:479px)" class="logo" type="image/jpg" srcset="img/logo_corte7.png">
+      
+      
+      <source media="(min-width:319px)" class="logo" type="image/webp" srcset="img/logo_corte8.webp">
+      <source media="(max-width:319px)" class="logo" type="image/jpg" srcset="img/logo_corte8.png">
+      
+   
+        
+      <img src="" class="imgHeader" alt="Logo">  
+   
+      
+               
+      
+      
+      
+      
+      </picture> 
+                
+                
+
+                
+                    
+                    
+                
+                
+                
+                
+                 <nav>
+                     
+                     
+                     
+                    <ul id="segBotoesGeral">
+                        
+                        <li class="segBotoes">
+                            <a href="index.php"  class="btnSelect" >HOME</a>
+                            
+                        </li>
+                        
+                        <li class="segBotoes">
+                            <a href="cartaoDeVisita.php"  class="btnSelect" >CARDÁPIO</a>
+                            
+                        </li>
+                        
+                        <li class="segBotoes">
+                            <a href="folhetos.php" class="btnSelect" >QUEM SOMOS</a>
+                            
+                        </li>
+                        
+                        <li class="segBotoes">
+                            <a href="faixas.php" class="btnSelect" >DELIVERY</a>
+                            
+                        </li>
+                        
+                        
+                    </ul> 
+                    
+                    
+                </nav>
+                
+               
+                
+                <section id="segFita">
+                    <a href="">
+                        <img src="img/fita1.png" alt="fita" id="fita1">
+                        <div id="espacoFita"><h2 id="txtFita">Pedir Online</h2></div>
+                        <img src="img/fita2.png" alt="fita" id="fita2">
+                    </a>    
+                    
+                </section>
+                
+                
+                
+            </header>
+            
+            
+            
+            
+            
+            
+            <main>
+                
+                
+                <div id="faixaCardapio"><h2 id="txtFaixaCardapio">Pagina do Usuario</h2></div>
+                
+                <section id="segUsuario">
+                    
+                    
+                   
+                    <section id="segDados">
+                       
+                    </section>
+                    
+                    <section id="segPedidoAberto">
+                      
+                    </section>
+                    
+                    <section id="segPizzaMaisPedida">
+                        
+                      
+                    </section>
+                    
+                    <section id="segHistoricoPizzas">
+                        
+                    </section>
+                    
+                    
+                </section>
+            </main>
+            
+            
+            
+            
+            
+            <footer>
+                
+                <picture>
+          
+      
+      <source media="(min-width:1366px)"  type="image/webp" srcset="img/header.jpg">    
+      <source media="(min-width:1366px)" class="logo" type="image/jpg" srcset="img/logo_corte2.png">
+      
+      
+      <source media="(min-width:1199px)" class="logo" type="image/webp" srcset="img/logo_corte3.webp">
+      <source media="(min-width:1199px)" class="logo" type="image/jpg" srcset="img/logo_corte3.png">
+      
+      
+      <source media="(min-width:1023px)" class="logo" type="image/webp" srcset="img/logo_corte4.webp">
+      <source media="(min-width:1023px)" class="logo" type="image/jpg" srcset="img/logo_corte4.png">
+      
+      
+      <source media="(min-width:767px)" class="logo" type="image/webp" srcset="img/logo_corte5.webp">
+      <source media="(min-width:767px)" class="logo" type="image/jpg" srcset="img/logo_corte5.png">
+      
+      
+      <source media="(min-width:599px)" class="logo" type="image/webp" srcset="img/logo_corte6.webp">
+      <source media="(min-width:599px)" class="logo" type="image/jpg" srcset="img/logo_corte6.png">
+      
+      
+      <source media="(min-width:479px)" class="logo" type="image/webp" srcset="img/logo_corte7.webp">
+      <source media="(min-width:479px)" class="logo" type="image/jpg" srcset="img/logo_corte7.png">
+      
+      
+      <source media="(min-width:319px)" class="logo" type="image/webp" srcset="img/logo_corte8.webp">
+      <source media="(max-width:319px)" class="logo" type="image/jpg" srcset="img/logo_corte8.png">
+      
+   
+        
+      <img src="" class="imgFooter" alt="Logo">  
+   
+      
+               
+      
+      
+      
+      
+      </picture> 
+                
+                
+                <section id="segItensRodape">
+                    
+                    <section id="segHorario">
+                        <h3>Horário de funcionamento</h3>
+                        <p id="txtHorario">TERÇA À SEXTA DÁS 18:00 ÀS 00:00 SÁBADOS, DOMINGOS E FERIADOS DAS 18:00 ÀS 01:00</p>
+                    </section>
+                
+                
+                
+                    <section id="segEnd">
+                        <p id="txtLocal">AV. DOS AUTONOMISTAS N° 4.134, BARUERI - SP </p>
+                    </section>
+                
+                <iframe>
+                    
+                </iframe>
+                
+                    <article id="segCartoes">
+                        <h3 id="aceitamos">Aceitamos:</h3>
+                        <img src="img/cartao1.png" alt="Bandeira" class="imgBandRodape">
+                        <img src="img/cartao2.png" alt="Bandeira" class="imgBandRodape">
+                        <img src="img/cartao3.png" alt="Bandeira" class="imgBandRodape">
+                    </article>
+                    
+                </section> 
+                
+                
+                
+                
+                <article id="barraFinalFooter">
+                    <a href=""><img src="img/face.png" alt="Rede Social" class="imgRedeRodape"></a>
+                    <a href=""><img src="img/insta.png" alt="Rede Social" class="imgRedeRodape"></a>
+                    <a href=""><img src="img/tiw.png" alt="Rede Social" class="imgRedeRodape"></a>
+                    <p id="txtDireitos"> La Prima &COPY; Todos Direitos Reservados.</p>
+                </article>
+                
+            </footer>
+           
+        </section>
+    </body>
+</html>
